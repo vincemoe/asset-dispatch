287221ca248928df8f9cf32616f943abbbf049b1
Vincent Moeykens <vincent.moeykens@pb.com>
2018-05-09 22:37:39 -0400

    Implements state managing and navigation links dependant on sign in function

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
     BrowserRouter as Router, Route,
 } from 'react-router-dom';
@@ -13,39 +13,60 @@ import HomePage from './Home';
 import AccountPage from './Account';
 
 import * as routes from '../constants/routes';
+import {firebase} from '../firebase';
 
-const App = (() =>
-        <Router>
-            <div>
-                <Navigation/>
-                <hr/>
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-                <Route
-                    exact path={routes.LANDING}
-                    component={() => <LandingPage/>}
-                />
-                <Route
-                    exact path={routes.SIGN_UP}
-                    component={() => <SignUpPage/>}
-                />
-                <Route
-                    exact path={routes.SIGN_IN}
-                    component={() => <SignInPage/>}
-                />
-                <Route
-                    exact path={routes.PASSWORD_FORGET}
-                    component={() => <PasswordForgetPage/>}
-                />
-                <Route
-                    exact path={routes.HOME}
-                    component={() => <HomePage/>}
-                />
-                <Route
-                    exact path={routes.ACCOUNT}
-                    component={() => <AccountPage/>}
-                />
-            </div>
-        </Router>
-);
+        this.state = {
+            authUser: null,
+        };
+    };
+
+    componentDidMount() {
+        firebase.auth.onAuthStateChanged(authUser => {
+            authUser
+                ? this.setState(() => ({authUser}))
+                : this.setState(() => ({authUser: null}));
+        });
+    };
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Navigation authUser={this.state.authUser} />
+                    <hr/>
+
+                    <Route
+                        exact path={routes.LANDING}
+                        component={() => <LandingPage/>}
+                    />
+                    <Route
+                        exact path={routes.SIGN_UP}
+                        component={() => <SignUpPage/>}
+                    />
+                    <Route
+                        exact path={routes.SIGN_IN}
+                        component={() => <SignInPage/>}
+                    />
+                    <Route
+                        exact path={routes.PASSWORD_FORGET}
+                        component={() => <PasswordForgetPage/>}
+                    />
+                    <Route
+                        exact path={routes.HOME}
+                        component={() => <HomePage/>}
+                    />
+                    <Route
+                        exact path={routes.ACCOUNT}
+                        component={() => <AccountPage/>}
+                    />
+                </div>
+            </Router>
+        );
+    }
+}
 
 export default App;
