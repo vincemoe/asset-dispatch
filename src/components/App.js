eb8293e96b6affc0f3bd92dc43088d72b7d560f5
Vincent Moeykens <vincent.moeykens@pb.com>
2018-05-10 22:36:19 -0400

    Adds map and material design

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {
     BrowserRouter as Router, Route,
 } from 'react-router-dom';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 import Navigation from './Navigation';
 import LandingPage from './Landing';
@@ -13,14 +14,59 @@ import HomePage from './Home';
 import AccountPage from './Account';
 
 import * as routes from '../constants/routes';
-import {firebase} from '../firebase';
+import {auth, firebase} from '../firebase';
+
+/*UI Components*/
+
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+import SignOutButton from './SignOut';
+
+class Login extends Component {
+    static muiName = 'FlatButton';
+
+    render() {
+        return (
+            <FlatButton {...this.props} containerElement={<Link to={routes.SIGN_IN}/>} label="Login"/>
+        );
+    }
+}
+
+const Logged = (props) => (
+    <IconMenu
+        {...props}
+        iconButtonElement={
+            <IconButton><MoreVertIcon/></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+    >
+        <Link to={routes.ACCOUNT}><MenuItem primaryText="Account"/></Link>
+        <MenuItem primaryText="Help"/>
+        <SignOutButton/>
+    </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
 
 class App extends Component {
+
+    static muiName = 'FlatButton';
+
     constructor(props) {
         super(props);
 
         this.state = {
             authUser: null,
+            open: false
         };
     };
 
@@ -36,35 +82,44 @@ class App extends Component {
         return (
             <Router>
                 <div>
-                    <Navigation authUser={this.state.authUser} />
-                    <hr/>
+                <MuiThemeProvider>
+                    <AppBar
+                        title="Asset Dispatch"
+                        iconElementLeft={<span/>}
+                        iconElementRight={this.state.authUser ? <Logged/> : <Login/>}
+                    /></MuiThemeProvider>
 
-                    <Route
-                        exact path={routes.LANDING}
-                        component={() => <LandingPage/>}
-                    />
-                    <Route
-                        exact path={routes.SIGN_UP}
-                        component={() => <SignUpPage/>}
-                    />
-                    <Route
-                        exact path={routes.SIGN_IN}
-                        component={() => <SignInPage/>}
-                    />
-                    <Route
-                        exact path={routes.PASSWORD_FORGET}
-                        component={() => <PasswordForgetPage/>}
-                    />
-                    <Route
-                        exact path={routes.HOME}
-                        component={() => <HomePage/>}
-                    />
-                    <Route
-                        exact path={routes.ACCOUNT}
-                        component={() => <AccountPage/>}
-                    />
+                   <div>
+                        <Route
+                            exact path={routes.LANDING}
+                            component={() => <div style={{position: 'absolute', top: '0', bottom: '0', left: '0', right: '0', marginTop: '-25p', height: '100vh'}}>
+                                <LandingPage/></div>}
+                        />
+                        <Route
+                            exact path={routes.SIGN_UP}
+                            component={() => <SignUpPage/>}
+                        />
+                        <Route
+                            exact path={routes.SIGN_IN}
+                            component={() =>
+                                <SignInPage/>}
+                        />
+                        <Route
+                            exact path={routes.PASSWORD_FORGET}
+                            component={() => <PasswordForgetPage/>}
+                        />
+                        <Route
+                            exact path={routes.HOME}
+                            component={() => <HomePage/>}
+                        />
+                        <Route
+                            exact path={routes.ACCOUNT}
+                            component={() => <AccountPage/>}
+                        />
+                    </div>
+
                 </div>
-            </Router>
+                </Router>
         );
     }
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, {Component} from 'react';
+import {withRouter, Route} from 'react-router-dom';
 
-import { SignUpLink } from './SignUp';
-import { auth } from '../firebase';
+import {SignUpLink} from './SignUp';
+import {auth} from '../firebase';
 import * as routes from '../constants/routes';
+import Dialog from 'material-ui/Dialog';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Link } from 'react-router-dom';
 
-const SignInPage = (({ history }) =>
+
+const SignInPage = (({history}) =>
     <div>
-        <h1>SignIn</h1>
-        <SignInForm history={history} />
-        <SignUpLink />
+        <SignInForm history={history}/>
     </div>);
 
 const byPropKey = (propertyName, value) => () => ({
@@ -26,8 +30,17 @@ class SignInForm extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { ...INITIAL_STATE };
+        this.state = {open: true, ...INITIAL_STATE};
     }
+
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
 
     onSubmit = ((event) => {
         const {
@@ -41,7 +54,7 @@ class SignInForm extends Component {
 
         auth.doSignInWithEmailAndPassword(email, password)
             .then(() => {
-                this.setState(() => ({ ...INITIAL_STATE }));
+                this.setState(() => ({...INITIAL_STATE}));
                 history.push(routes.HOME);
             })
             .catch(error => {
@@ -62,26 +75,50 @@ class SignInForm extends Component {
             password === '' ||
             email === '';
 
-        return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    value={email}
-                    onChange={event => this.setState(byPropKey('email', event.target.value))}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <input
-                    value={password}
-                    onChange={event => this.setState(byPropKey('password', event.target.value))}
-                    type="password"
-                    placeholder="Password"
-                />
-                <button disabled={isInvalid} type="submit">
-                    Sign In
-                </button>
+        const actions = [
+            <Link to={routes.LANDING}>
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+            /></Link>,
 
-                { error && <p>{error.message}</p> }
-            </form>
+        ];
+
+        return (
+            <MuiThemeProvider>
+                <Dialog
+                    title="Sign In"
+                    actions={actions}
+                    modal={true}
+                    open={this.state.open}
+                >
+                    <form onSubmit={this.onSubmit}>
+                        <input
+                            value={email}
+                            onChange={event => this.setState(byPropKey('email', event.target.value))}
+                            type="text"
+                            placeholder="Email Address"
+                        />
+                        <input
+                            value={password}
+                            onChange={event => this.setState(byPropKey('password', event.target.value))}
+                            type="password"
+                            placeholder="Password"
+                        />
+                        <FlatButton
+                            label="Submit"
+                            primary={true}
+                            type="submit"
+                            disabled={isInvalid}
+                        />
+                        <SignUpLink/>
+
+                        {error && <p>{error.message}</p>}
+                    </form>
+                </Dialog>
+            </MuiThemeProvider>
+
         );
     }
 }
