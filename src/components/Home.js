2438e8c0be039f744f162b0a2a5245791ef9c8f3
Vincent Moeykens <vpam99@gmail.com>
2018-05-28 22:22:43 -0400

    More UI Updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
 import withAuthorization from './withAuthorization';
+import Sidebar from './Sidebar.js';
 
 class Home extends Component {
 
@@ -39,7 +40,10 @@ class Home extends Component {
                     {...this.state.viewport}
                     onViewportChange={(viewport) => this.setState({viewport})}
                     mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
-                />
+                >
+                    <Sidebar/>
+                </ReactMapGL>
+
             </div>
         );
     }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import icons from 'glyphicons'
+
+import {Card, CardBody, CardSubtitle, CardText, CardTitle, Button, Container, Row, Col} from 'reactstrap';
+
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <div style={{position: 'absolute',
+                top: '0',
+                right: '0',
+                maxWidth: '400px',
+                background: '#fff',
+                paddingLeft: '12px 24px',
+                margin: '20px',
+                fontSize: '13px',
+                lineHeight: '2',
+                color: '#6b6b76',}}>
+                <Card>
+                    <CardBody>
+                        <Container>
+                            <Row>
+                                <Col style={{paddingBottom: '10px'}}>
+                                    <Button color="primary" size="lg">New Asset {icons.plus}</Button>{' '}
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Card>
+                                        <CardBody>
+                                            <CardTitle>Test</CardTitle>
+                                            <CardSubtitle>Account Information</CardSubtitle>
+                                            <CardText>Account info or something here.</CardText>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
